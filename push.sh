d394dd2131c44dada1d4043bc631d35975617e2c
NR-SkaterBoy <nr.rick.dev@gmail.com>
2021-12-23 20:56:31 +0100

    Update JSON

--- push.sh
+++ push.sh
@@ -1,7 +1,9 @@
 #!bin/bash
 
+cd /home/ricsi/Dokumentumok/Discord/MCBOT-json/
+
 git init
 git add .
 git commit -m "Update JSON"
 git remote add origin 'git@github.com:NR-SkaterBoy/MCBOT-json.git'
-git push
+#git push
