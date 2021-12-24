5792f20fbf742702c2f7552f7eb531a0c68b82d9
Neuwald Ricsi <43517856+NR-SkaterBoy@users.noreply.github.com>
2021-12-24 16:02:23 +0100

    Delete push.sh

--- push.sh
+++
@@ -1,9 +0,0 @@
-#!bin/bash
-
-cd /home/ricsi/Dokumentumok/Discord/MCBOT-json/
-
-git init
-git add .
-git commit -m "Update JSON"
-git remote add origin 'git@github.com:NR-SkaterBoy/MCBOT-json.git'
-git push
